048eb6de4d5052af0b4b114db98e74edc0e167a1
TuanAnh <tuananh1905>
2023-10-19 23:20:56 +0700

    a

--- src/lambda-functions/patient/get_a_patient/app.py
+++ src/lambda-functions/patient/get_a_patient/app.py
@@ -1,6 +1,7 @@
 import json
 import pymysql
 import os
+import datetime
 
 conn = pymysql.connect(host=os.environ.get('HOST'), user=os.environ.get(
     'USERNAME'), passwd=os.environ.get('PASSWORD'), db=os.environ.get('DATABASE'))
@@ -19,12 +20,22 @@ def lambda_handler(event, context):
     patient_id = event['pathParameters']['id']
     query = "SELECT * FROM `patient` WHERE patient_id = %s;"
     cursor.execute(query, (patient_id))
-    rows = cursor.fetchall()
+
+    column_names = [desc[0] for desc in cursor.description]
+
+    result = []
+    for row in cursor.fetchall():
+        row_list = list(row)
+        if isinstance(row_list[11], (datetime.date, datetime.datetime)):
+            row_list[11] = row_list[11].isoformat()
+
+        result.append(dict(zip(column_names, row_list)))
+
     cursor.close()
     conn.close()
 
     return {
         'statusCode': 200,
         'headers': {},
-        'body': json.dumps(rows)
+        'body': json.dumps(result)
     }
