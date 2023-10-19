1fbe13a440a8f5f2922c931c5192ee22e23d03ed
TuanAnh <tuananh1905>
2023-10-19 22:00:24 +0700

    a

--- database/create_database.sql
+++ database/create_database.sql
@@ -1,22 +1,20 @@
-IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'nguyen-tran-clinic')
-BEGIN
-    CREATE DATABASE [nguyen-tran-clinic];
-END
+CREATE DATABASE IF NOT EXISTS `nguyen_tran_clinic`;
 
-USE [nguyen-tran-clinic]
+USE `nguyen_tran_clinic`;
 
-CREATE TABLE Patient (
-    PatientID INT IDENTITY(1,1) PRIMARY KEY,
-    PatientName NVARCHAR(255) NOT NULL,
-    DateOfBirth DATE,
-    Gender BIT, 
-    PhoneNumber NVARCHAR(15), 
-    FullMedicalHistory NVARCHAR(MAX),
-    DentalMedicalHistory NVARCHAR(MAX),
-    Email NVARCHAR(255),
-    Address NVARCHAR(500),
-    Description NVARCHAR(MAX),
-    CreatedDate DATETIME DEFAULT GETDATE()
+CREATE TABLE `patient` (
+    `patient_id` INT AUTO_INCREMENT PRIMARY KEY,
+    `patient_name` VARCHAR(255) NOT NULL,
+    `date_of_birth` DATE,
+    `gender` TINYINT, 
+    `phone_number` VARCHAR(15) NOT NULL, 
+    `full_medical_history` TEXT,
+    `dental_medical_history` TEXT,
+    `email` VARCHAR(255),
+    `address` VARCHAR(500),
+    `description` TEXT,
+    `profile_image` VARCHAR(100),
+    `created_date` DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE INDEX idx_PatientName ON Patient (PatientName);
+CREATE INDEX `idx_patient_name` ON `patient`(`patient_name`);
