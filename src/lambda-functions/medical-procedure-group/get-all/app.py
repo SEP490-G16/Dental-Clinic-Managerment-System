9788d0b0802e7b6d8b829deea95b871fe19fcf13
nguyengiantuananh <nguyengiantuananh@gmail.com>
2023-10-30 08:45:11 +0700

    a

--- src/lambda-functions/medical-procedure-group/get-all/app.py
+++ src/lambda-functions/medical-procedure-group/get-all/app.py
@@ -17,26 +17,58 @@ def transform_row(row):
             transformed_row.append(value)
     return tuple(transformed_row)
 
+def get_mysql_error_message(error_code):
+    error_messages = {
+        1045: "Access denied for user",
+        1049: "Unknown database",
+        1146: "Table doesn't exist",
+        1452: "Foreign key constraint fails", 
+        1062: "Duplicate entry",
+        1054: "Unknown column in field list"
+    }
+    return error_messages.get(error_code, "Unknown MySQL error")
+
+
+def create_response(status_code, message, data=None, exception_type=None):
+    response_body = {
+        'message': message
+    }
+
+    if data is not None:
+        response_body['data'] = data
+    if exception_type is not None:
+        response_body['type'] = exception_type
+
+    return {
+        'statusCode': status_code,
+        'headers': {
+            "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Access-Control-Allow-Methods, Accept, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": "true",
+            "Access-Control-Allow-Methods": "POST, PUT, PATCH, GET, DELETE, OPTIONS"
+        },
+        'body': json.dumps(response_body, ensure_ascii=False)
+    }
 
 def lambda_handler(event, context):
     global conn, cursor
+    try:
+        query = "SELECT * FROM `medical_procedure_group` WHERE `active` != 0;"
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        column_names = [column[0] for column in cursor.description]
+        transformed_rows = [
+            dict(zip(column_names, transform_row(row))) for row in rows]
 
-    query = "SELECT * FROM `medical_procedure_group` WHERE `active` != 0;"
-    cursor.execute(query)
-    rows = cursor.fetchall()
-    column_names = [column[0] for column in cursor.description]
-    transformed_rows = [
-        dict(zip(column_names, transform_row(row))) for row in rows]
+        if len(transformed_rows) == 0:
+            return create_response(404, 'Medical procedure group not found')
 
-    if len(transformed_rows) == 0:
-        return {
-            'statusCode': 404,
-            'headers': {},
-            'body': json.dumps({'message': 'medical procedure group not found'}, ensure_ascii=False)
-        }
-
-    return {
-        'statusCode': 200,
-        'headers': {},
-        'body': json.dumps(transformed_rows, ensure_ascii=False)
-    }
+        return create_response(200, '', transformed_rows)
+    except pymysql.MySQLError as e:
+        print("MySQL error:", e)
+        error_message = get_mysql_error_message(e.args[0])
+        status_code = 400 if e.args[0] in [1452, 1062, 1054] else 500
+        return create_response(status_code, error_message, None, str(e.__class__.__name__))
+    except Exception as e:
+        print("Error:", e)
+        return create_response(500, 'Internal error', None, str(e.__class__.__name__))
