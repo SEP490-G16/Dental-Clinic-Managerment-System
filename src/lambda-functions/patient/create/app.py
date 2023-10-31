2e9bc080971672c81568f58cc217ddc0ae3d53d0
nguyengiantuananh <nguyengiantuananh@gmail.com>
2023-10-31 13:03:08 +0700

    a

--- src/lambda-functions/patient/create/app.py
+++ src/lambda-functions/patient/create/app.py
@@ -3,26 +3,52 @@ import pymysql
 import os
 import datetime
 
-conn = pymysql.connect(host=os.environ.get('HOST'),
-                       user=os.environ.get('USERNAME'),
-                       passwd=os.environ.get('PASSWORD'),
-                       db=os.environ.get('DATABASE'))
-cursor = conn.cursor()
-
 
 def get_value_or_none(data, key):
     return data[key] if key in data else None
 
+def get_mysql_error_message(error_code):
+    error_messages = {
+        1045: "Access denied for user",
+        1049: "Unknown database",
+        1146: "Table doesn't exist",
+        1452: "Foreign key constraint fails", 
+        1062: "Duplicate entry",
+        1054: "Unknown column in field list"
+    }
+    return error_messages.get(error_code, "Unknown MySQL error")
+
+
+def create_response(status_code, message, data=None, exception_type=None):
+    response_body = {
+        'message': message
+    }
+
+    if data is not None:
+        response_body['data'] = data
+    if exception_type is not None:
+        response_body['type'] = exception_type
+
+    return {
+        'statusCode': status_code,
+        'headers': {
+            "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Access-Control-Allow-Methods, Accept, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": "true",
+            "Access-Control-Allow-Methods": "POST, PUT, PATCH, GET, DELETE, OPTIONS"
+        },
+        'body': json.dumps(response_body, ensure_ascii=False)
+    }
 
 def lambda_handler(event, context):
-    global conn, cursor
-
+    # global conn, cursor
+    conn = pymysql.connect(host=os.environ.get('HOST'),
+                       user=os.environ.get('USERNAME'),
+                       passwd=os.environ.get('PASSWORD'),
+                       db=os.environ.get('DATABASE'))
+    cursor = conn.cursor()
     if event['httpMethod'] != 'POST' or not event.get('body'):
-        return {
-            'statusCode': 400,
-            'headers': {},
-            'body': json.dumps({'message': 'Bad Request'})
-        }
+        return create_response(400, 'Bad Request')
 
     try:
         data = json.loads(event['body'])
@@ -34,11 +60,7 @@ def lambda_handler(event, context):
             field for field in required_fields if not data.get(field)]
 
         if missing_fields:
-            return {
-                'statusCode': 400,
-                'headers': {},
-                'body': json.dumps({'message': f"Fields {', '.join(missing_fields)} are required"})
-            }
+            return create_response(400, f"Fields {', '.join(missing_fields)} are required")
 
         query = """INSERT INTO `patient` (`patient_name`, `date_of_birth`, `gender`, `phone_number`, `full_medical_history`, 
                 `dental_medical_history`, `email`, `address`, `description`, `profile_image`)
@@ -58,22 +80,25 @@ def lambda_handler(event, context):
 
         conn.commit()
 
+        # Lấy ID của bản ghi vừa được tạo
+        cursor.execute("SELECT LAST_INSERT_ID();")
+        patient_id = cursor.fetchone()[0]
+
         return {
             'statusCode': 201,
-            'headers': {},
-            'body': json.dumps({'message': 'Patient created successfully'})
+            'headers': {
+                "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Access-Control-Allow-Methods, Accept, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": "true",
+                "Access-Control-Allow-Methods": "POST, PUT, PATCH, GET, DELETE, OPTIONS"
+            },
+            'body': json.dumps({'message': 'Patient created successfully', 'patient_id': patient_id})
         }
     except pymysql.MySQLError as e:
         print("MySQL error:", e)
-        return {
-            'statusCode': 500,
-            'headers': {},
-            'body': json.dumps({'message': 'Database error', 'type': str(e.__class__.__name__)})
-        }
+        error_message = get_mysql_error_message(e.args[0])
+        status_code = 400 if e.args[0] in [1452, 1062, 1054] else 500
+        return create_response(status_code, error_message, None, str(e.__class__.__name__))
     except Exception as e:
         print("Error:", e)
-        return {
-            'statusCode': 500,
-            'headers': {},
-            'body': json.dumps({'message': 'Internal error', 'type': str(e.__class__.__name__)})
-        }
+        return create_response(500, 'Internal error', None, str(e.__class__.__name__))
