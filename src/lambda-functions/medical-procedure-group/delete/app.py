70d066e177716fea988dee17eda6cc4070204ec2
TuanAnh <tuananh1905>
2023-10-31 20:56:43 +0700

    a

--- src/lambda-functions/medical-procedure-group/delete/app.py
+++ src/lambda-functions/medical-procedure-group/delete/app.py
@@ -2,57 +2,58 @@ import json
 import pymysql
 import os
 
-conn = pymysql.connect(host=os.environ.get('HOST'),
-                       user=os.environ.get('USERNAME'),
-                       passwd=os.environ.get('PASSWORD'),
-                       db=os.environ.get('DATABASE'))
-cursor = conn.cursor()
+def get_mysql_error_message(error_code):
+    error_messages = {
+        1045: "Access denied for user",
+        1049: "Unknown database",
+        1146: "Table doesn't exist",
+        1452: "Foreign key constraint fails", 
+        1062: "Duplicate entry",
+        1054: "Unknown column in field list"
+    }
+    return error_messages.get(error_code, "Unknown MySQL error")
 
+
+def create_response(status_code, message, data=None, exception_type=None):
+    response_body = {
+        'message': message
+    }
+
+    if data is not None:
+        response_body['data'] = data
+    if exception_type is not None:
+        response_body['type'] = exception_type
+
+    return {
+        'statusCode': status_code,
+        'headers': {
+            "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Access-Control-Allow-Methods, Accept, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": "true",
+            "Access-Control-Allow-Methods": "POST, PUT, PATCH, GET, DELETE, OPTIONS"
+        },
+        'body': json.dumps(response_body, ensure_ascii=False)
+    }
 
 def lambda_handler(event, context):
-    global conn, cursor
-
-    # Kiểm tra HTTP method và pathParameters
+    conn = pymysql.connect(host=os.environ.get('HOST'), user=os.environ.get('USERNAME'), passwd=os.environ.get('PASSWORD'), db=os.environ.get('DATABASE'))
+    cursor = conn.cursor()
     if event['httpMethod'] != 'DELETE' or not event.get('pathParameters') or 'id' not in event['pathParameters']:
-        return {
-            'statusCode': 400,
-            'headers': {},
-            'body': json.dumps({'message': 'Bad Request'})
-        }
+        return create_response(status_code=400, message='Bad Request')
 
     try:
         id = event['pathParameters']['id']
-
-        # Cập nhật trường `active` của bản ghi patient thành 0
-        query = "UPDATE `medical_procedure_group` SET `active`=0 WHERE patient_id=%s;"
-
+        query = "UPDATE `medical_procedure_group` SET `active`=0 WHERE medical_procedure_group_id=%s;"
         cursor.execute(query, (id,))
-
         conn.commit()
-
-        if cursor.rowcount == 0:  # Nếu không có bản ghi nào được cập nhật
-            return {
-                'statusCode': 404,
-                'headers': {},
-                'body': json.dumps({'message': 'medical procedure group not found'})
-            }
-
-        return {
-            'statusCode': 200,
-            'headers': {},
-            'body': json.dumps({'message': 'medical procedure group deactivated successfully'})
-        }
+        if cursor.rowcount == 0:
+            return create_response(status_code=404, message='medical procedure group not found') 
+        return create_response(status_code=200, message='Medical procedure group deactivated successfully')
     except pymysql.MySQLError as e:
         print("MySQL error:", e)
-        return {
-            'statusCode': 500,
-            'headers': {},
-            'body': json.dumps({'message': 'Database error', 'type': str(e.__class__.__name__)})
-        }
+        error_message = get_mysql_error_message(e.args[0])
+        status_code = 400 if e.args[0] in [1452, 1062, 1054] else 500
+        return create_response(status_code, error_message, None, str(e.__class__.__name__))
     except Exception as e:
         print("Error:", e)
-        return {
-            'statusCode': 500,
-            'headers': {},
-            'body': json.dumps({'message': 'Internal error', 'type': str(e.__class__.__name__)})
-        }
+        return create_response(500, 'Internal error', None, str(e.__class__.__name__))
