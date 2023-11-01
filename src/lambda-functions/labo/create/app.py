a0ded891d9c760d6729ec885928f589ab3c1fbd1
TuanAnh <tuananh1905>
2023-11-01 10:23:21 +0700

    a

---
+++ src/lambda-functions/labo/create/app.py
@@ -0,0 +1,82 @@
+import json
+import pymysql
+import os
+import datetime
+
+def get_value_or_none(data, key):
+    return data[key] if key in data else None
+
+def get_mysql_error_message(error_code):
+    error_messages = {
+        1045: "Access denied for user",
+        1049: "Unknown database",
+        1146: "Table doesn't exist",
+        1452: "Foreign key constraint fails", 
+        1062: "Duplicate entry",
+        1054: "Unknown column in field list"
+    }
+    return error_messages.get(error_code, "Unknown MySQL error")
+
+
+def create_response(status_code, message, data=None, exception_type=None):
+    response_body = {
+        'message': message
+    }
+
+    if data is not None:
+        response_body['data'] = data
+    if exception_type is not None:
+        response_body['type'] = exception_type
+
+    return {
+        'statusCode': status_code,
+        'headers': {
+            "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Access-Control-Allow-Methods, Accept, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": "true",
+            "Access-Control-Allow-Methods": "POST, PUT, PATCH, GET, DELETE, OPTIONS"
+        },
+        'body': json.dumps(response_body, ensure_ascii=False)
+    }
+
+def lambda_handler(event, context):
+    # global conn, cursor
+    conn = pymysql.connect(host=os.environ.get('HOST'), user=os.environ.get('USERNAME'), passwd=os.environ.get('PASSWORD'), db=os.environ.get('DATABASE'))
+    cursor = conn.cursor()
+    if event['httpMethod'] != 'POST' or not event.get('body'):
+        return create_response(400, 'Bad Request')
+
+    try:
+        data = json.loads(event['body'])
+
+        required_fields = ['name', 'phone_number', 'address']
+
+        missing_fields = [
+            field for field in required_fields if not data.get(field)]
+
+        if missing_fields:
+            return create_response(400, f"Fields {', '.join(missing_fields)} are required")
+
+        query = """INSERT INTO `labo` (`name`, `description`, `phone_number	`, `email`, `address,)
+                VALUES (%s, %s, %s, %s, %s);"""
+
+        cursor.execute(query, (data.get('name'),
+                                get_value_or_none(data, 'description'),
+                                data.get('phone_number'),
+                                get_value_or_none(data, 'email'),
+                                data.get('address')))
+        
+        cursor.execute("SELECT labo_id FROM labo ORDER BY labo_id DESC LIMIT 1;")
+        row = cursor.fetchone()
+        id = row[0]
+        conn.commit()
+
+        return create_response(201, message='Labo created successfully', data= {'labo_id': id})
+    except pymysql.MySQLError as e:
+        print("MySQL error:", e)
+        error_message = get_mysql_error_message(e.args[0])
+        status_code = 400 if e.args[0] in [1452, 1062, 1054] else 500
+        return create_response(status_code, error_message, None, str(e.__class__.__name__))
+    except Exception as e:
+        print("Error:", e)
+        return create_response(500, 'Internal error', None, str(e.__class__.__name__))
---
+++ src/lambda-functions/labo/get-all/app.py
@@ -0,0 +1,74 @@
+import json
+import pymysql
+import os
+import datetime
+
+conn = pymysql.connect(host=os.environ.get('HOST'), user=os.environ.get(
+    'USERNAME'), passwd=os.environ.get('PASSWORD'), db=os.environ.get('DATABASE'))
+cursor = conn.cursor()
+
+
+def transform_row(row):
+    transformed_row = []
+    for value in row:
+        if isinstance(value, datetime.date):
+            transformed_row.append(str(value))
+        else:
+            transformed_row.append(value)
+    return tuple(transformed_row)
+
+def get_mysql_error_message(error_code):
+    error_messages = {
+        1045: "Access denied for user",
+        1049: "Unknown database",
+        1146: "Table doesn't exist",
+        1452: "Foreign key constraint fails", 
+        1062: "Duplicate entry",
+        1054: "Unknown column in field list"
+    }
+    return error_messages.get(error_code, "Unknown MySQL error")
+
+
+def create_response(status_code, message, data=None, exception_type=None):
+    response_body = {
+        'message': message
+    }
+
+    if data is not None:
+        response_body['data'] = data
+    if exception_type is not None:
+        response_body['type'] = exception_type
+
+    return {
+        'statusCode': status_code,
+        'headers': {
+            "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Access-Control-Allow-Methods, Accept, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": "true",
+            "Access-Control-Allow-Methods": "POST, PUT, PATCH, GET, DELETE, OPTIONS"
+        },
+        'body': json.dumps(response_body, ensure_ascii=False)
+    }
+
+def lambda_handler(event, context):
+    global conn, cursor
+    try:
+        query = "SELECT * FROM `labo` WHERE `active` != 0;"
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        column_names = [column[0] for column in cursor.description]
+        transformed_rows = [
+            dict(zip(column_names, transform_row(row))) for row in rows]
+
+        if len(transformed_rows) == 0:
+            return create_response(404, 'Labo not found')
+
+        return create_response(200, '', transformed_rows)
+    except pymysql.MySQLError as e:
+        print("MySQL error:", e)
+        error_message = get_mysql_error_message(e.args[0])
+        status_code = 400 if e.args[0] in [1452, 1062, 1054] else 500
+        return create_response(status_code, error_message, None, str(e.__class__.__name__))
+    except Exception as e:
+        print("Error:", e)
+        return create_response(500, 'Internal error', None, str(e.__class__.__name__))
