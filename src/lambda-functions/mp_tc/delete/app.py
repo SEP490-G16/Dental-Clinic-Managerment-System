f7367260c6fbc49a5bddc5346e7664906fd502be
nguyengiantuananh <nguyengiantuananh@gmail.com>
2023-11-15 17:02:15 +0700

    a

---
+++ src/lambda-functions/mp_tc/delete/app.py
@@ -0,0 +1,83 @@
+import json
+import pymysql
+import os
+import datetime
+
+def get_value_or_none(data, key):
+    return data[key] if key in data else None
+
+def get_mysql_error_message(error_code):
+    error_messages = {
+        1045: "Access denied for user",
+        1049: "Unknown database",
+        1146: "Table doesn't exist",
+        1452: "Foreign key constraint fails", 
+        1062: "Duplicate entry",
+        1054: "Unknown column in field list"
+    }
+    return error_messages.get(error_code, "Unknown MySQL error")
+
+
+def create_response(status_code, message, data=None, exception_type=None):
+    response_body = {
+        'message': message
+    }
+
+    if data is not None:
+        response_body['data'] = data
+    if exception_type is not None:
+        response_body['type'] = exception_type
+
+    return {
+        'statusCode': status_code,
+        'headers': {
+            "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Access-Control-Allow-Methods, Accept, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": "true",
+            "Access-Control-Allow-Methods": "POST, PUT, PATCH, GET, DELETE, OPTIONS"
+        },
+        'body': json.dumps(response_body, ensure_ascii=False)
+    }
+
+def lambda_handler(event, context):
+    conn = None
+    cursor = None
+    response = create_response(500, 'Internal error', None)
+    
+    if event['httpMethod'] != 'PUT' or not event.get('body') or not event.get('pathParameters') or 'treatment-course-id' not in event['pathParameters'] or 'medical-procedure-id' not in event['pathParameters']:
+        return create_response(400, 'Bad Request')
+
+    try:
+        t_id = event['pathParameters']['treatment-course-id']
+        m_id = event['pathParameters']['medical-procedure-id']
+        data = json.loads(event['body'])
+
+        conn = pymysql.connect(host=os.environ.get('HOST'), user=os.environ.get('USERNAME'), passwd=os.environ.get('PASSWORD'), db=os.environ.get('DATABASE'))
+        cursor = conn.cursor()
+        query = """
+            UPDATE `mp_tc` 
+            SET `status` = 0
+            WHERE treatment_course_id=%s AND medical_procedure_id=%s;
+            """
+        cursor.execute(query, (t_id,
+                               m_id))
+
+        conn.commit()
+        if cursor.rowcount == 0:
+            response =  create_response(status_code=404, message='Treatment course not found or not change')
+        else:
+            response = create_response(status_code=200, message='Treatment course deactivated successfully') 
+    except pymysql.MySQLError as e:
+        print("MySQL error:", e)
+        error_message = get_mysql_error_message(e.args[0])
+        status_code = 400 if e.args[0] in [1452, 1062, 1054] else 500
+        response = create_response(status_code, error_message, None, str(e.__class__.__name__))
+    except Exception as e:
+        print("Error:", e)
+        response = create_response(500, 'Internal error', None, str(e.__class__.__name__))
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+    return response
